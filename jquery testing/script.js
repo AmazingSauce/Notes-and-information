d5283999444147b13bc09e6d24d8fbcb05380b6b
AmazingSauce <AmazingSauce@gmail.com>
2015-05-28 16:58:10 +0100

    updating notes

--- jquery testing/script.js
+++ jquery testing/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+    $('div').slideDown('slow');
 
+    $(document).ready(function(){
+    $('div').mouseenter(function(){
+        $("div").fadeTo('fast', 1);
+    });
+    $('div').mouseleave(function(){
+        $("div").fadeTo('fast', 0.5);
+    });
+    
 
 });
